368f7d5f07cdaf8dcf74d59b095661e429762b61
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-10-24 19:41:09 -0400

    Update

---
+++ src/main/java/com/jay/bank/models/Bank.java
@@ -0,0 +1,63 @@
+package com.jay.bank.models;
+
+import javax.persistence.*;
+
+
+@Entity
+public class Bank {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+
+    private String name;
+    private String location;
+    private String phoneNumber;
+
+    // default constructor for JPA to use when creating an instance of this class from a database record (row)
+    public Bank() {
+    }
+
+    public Bank(String name, String location, String phoneNumber) {
+        this.name = name;
+        this.location = location;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getAreaCode() {
+        return phoneNumber.substring(0, 3);
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
