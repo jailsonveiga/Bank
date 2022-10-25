a3e8d463aa8f8418c24e6e3204082081cf995ed4
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-10-25 19:47:42 -0400

    @PostMapping EndPoint Create Banks

---
+++ src/main/java/com/jay/bank/controllers/BankController.java
@@ -0,0 +1,36 @@
+package com.jay.bank.controllers;
+
+import com.jay.bank.models.Bank;
+import com.jay.bank.repositories.BankRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/banks") // good practice for controllers that return data, not webpages
+public class BankController {
+
+    @Autowired
+    private BankRepository bankRepository;
+
+    @PostMapping
+    public ResponseEntity<?> addOneBankToDB(@RequestBody Bank newBankData) {
+        try {
+            // validation occurs here
+            // save the new bank to the database
+            Bank addedBank = bankRepository.save(newBankData);
+
+            // return the new bank to the client
+            // return ResponseEntity.ok(addedBank); // Status 200 Ok
+
+            return new ResponseEntity<>(addedBank, HttpStatus.CREATED); // Status 201 Created
+
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().body(e.getMessage()); // 500 error code
+        }
+    }
+}
