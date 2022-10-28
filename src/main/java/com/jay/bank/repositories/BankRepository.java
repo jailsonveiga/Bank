0f9037a91897cc20142aad693b1114c61d3a0070
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-10-27 20:13:31 -0400

    Update

--- src/main/java/com/jay/bank/repositories/BankRepository.java
+++ src/main/java/com/jay/bank/repositories/BankRepository.java
@@ -2,6 +2,20 @@ package com.jay.bank.repositories;
 
 import com.jay.bank.models.Bank;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface BankRepository extends JpaRepository<Bank, Long> {
+
+    Optional<Bank> findByName(String name);
+
+//    @Query("SELECT * FROM bank WHERE phone_number LIKE \"?1%\";")
+//    List<Bank> findAllAreaCodes(String areaCode);
+
+    @Query("SELECT b FROM Bank b WHERE b.phoneNumber LIKE ?1%")
+    List<Bank> getAllAreaCodes(String areaCode);
+
+
 }
