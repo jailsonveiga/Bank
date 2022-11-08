d83b799ca8d8a9e20f46f31a27e6ed6b184cff17
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-11-08 18:57:39 -0500

    Update

--- src/main/java/com/jay/bank/models/CheckingAccount.java
+++ src/main/java/com/jay/bank/models/CheckingAccount.java
@@ -1,4 +1,59 @@
 package com.jay.bank.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class CheckingAccount {
+
+    @Id() // primary key
+    @GeneratedValue (strategy = GenerationType.IDENTITY) // auto increment value for primary key (id) field in database table
+    private Long id;
+
+    private String alias;
+    private Long balance;
+    private Long fee;
+
+    public CheckingAccount() {
+    }
+
+    public CheckingAccount(String alias, Long balance, Long fee) {
+        this.alias = alias;
+        this.balance = balance;
+        this.fee = fee;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+
+    public Long getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Long balance) {
+        this.balance = balance;
+    }
+
+    public Long getFee() {
+        return fee;
+    }
+
+    public void setFee(Long fee) {
+        this.fee = fee;
+    }
 }
