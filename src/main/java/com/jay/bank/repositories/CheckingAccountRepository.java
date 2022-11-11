60525fb0f135ed933f73fcfe1980b930223748c1
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-11-10 19:33:59 -0500

    Update

--- src/main/java/com/jay/bank/repositories/CheckingAccountRepository.java
+++ src/main/java/com/jay/bank/repositories/CheckingAccountRepository.java
@@ -3,6 +3,10 @@ package com.jay.bank.repositories;
 import com.jay.bank.models.CheckingAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Set;
+
 public interface CheckingAccountRepository extends JpaRepository<CheckingAccount, Long> {
+
+    Set<CheckingAccount> findAllByCustomers_Bank_Id(Long bankId);
 }
 
