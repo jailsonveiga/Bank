6033307470b4fd77f8a64fa96369eb18950f2216
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-11-15 20:57:12 -0500

    Update

--- src/main/java/com/jay/bank/repositories/CustomerRepository.java
+++ src/main/java/com/jay/bank/repositories/CustomerRepository.java
@@ -4,9 +4,12 @@ import com.jay.bank.models.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CustomerRepository extends JpaRepository<Customer,Long> {
     List<Customer> findAllByLastName(String lastName);
 
     List<Customer> findAllByBank_id(Long id);
+
+    Optional<Customer> findByUser_username(String username);
 }
